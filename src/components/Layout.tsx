7a166185ca19d420ca3e62891e13b3b1f3064c0b
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2024-08-14 14:32:38 -0700

    added slack styling a little bit

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,24 @@
-import {ConversationManagerCS, type PublicChannelData} from "../cloudstate/chat-manager"
+import {
+  ConversationManagerCS,
+  type PublicChannelData,
+} from "../cloudstate/chat-manager";
 import { useCloud } from "freestyle-sh";
 import { Chat } from "./Chat";
 import { SideBar } from "./Sidebar";
 
-
 export function SlackLayout(props: {
-    chatRoomId: string;
-    baseChannels: PublicChannelData[];
+  chatRoomId: string;
+  baseChannels: PublicChannelData[];
 }) {
-    return (
-      <div className="grid grid-cols-[20rem_1fr] h-screen">
-          <SideBar baseChannels={props.baseChannels} chatRoomId={props.chatRoomId} />
-          <Chat chatRoomId={props.chatRoomId} />
+  return (
+    <div className="grid grid-cols-[20rem_1fr] h-screen overflow-hidden">
+      <SideBar
+        baseChannels={props.baseChannels}
+        chatRoomId={props.chatRoomId}
+      />
+      <div className="h-screen">
+        <Chat chatRoomId={props.chatRoomId} />
       </div>
-    );
-  }
+    </div>
+  );
+}
