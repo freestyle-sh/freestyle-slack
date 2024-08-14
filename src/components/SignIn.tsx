3048f8de53b15fbfd0a75ddc5a3bd0b1af1dd487
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2024-08-14 14:52:48 -0700

    sign in page styling upgrades

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -8,17 +8,11 @@ export function SignIn() {
   const [username, setUsername] = useState("");
 
   return (
-    <div className="grid max-w-lg mx-auto gap-2 mt-8">
-      <h1 className="text-2xl font-bold text-center mb-2">Sign In</h1>
-      <input
-        type="email"
-        placeholder="example@example.com"
-        onChange={(e) => setUsername(e.currentTarget.value)}
-        className="border py-1 px-2 rounded-md"
-      />
-      <button
-        className="bg-blue-500 hover:bg-blue-700 font-medium text-white py-1 px-2 rounded"
-        onClick={async () => {
+    <div className="flex h-screen items-center justify-center bg-[#5A2B5D]">
+      <form
+        className="grid max-w-lg mx-auto gap-2 bg-white p-6 rounded-lg shadow-sm min-w-[50vw]"
+        onSubmit={async (e) => {
+          e.preventDefault();
           // get passkey generation options
           const options = await auth.startRegistration(username);
 
@@ -31,8 +25,23 @@ export function SignIn() {
           window.location.href = "/";
         }}
       >
-        Login with Passkey
-      </button>
+        <h1 className="text-2xl font-bold text-center">Sign In</h1>
+        <p className="text-gray-600 text-center  mb-4">
+          Enter the name you want to use in the chat.
+        </p>
+        <input
+          type="name"
+          placeholder="Beyonce"
+          onChange={(e) => setUsername(e.currentTarget.value)}
+          className="border py-1 px-2 rounded-md mb-2"
+        />
+        <button
+          className="bg-[#5A2B5D] hover:bg-[#743878] font-medium text-white py-1 px-2 rounded transition-all"
+          type="submit"
+        >
+          Login with Passkey
+        </button>
+      </form>
     </div>
   );
 }
